a0d9fed1d13191ffc20e869c5a9d0d53b620ca8c
Universal Studio <uni@un1.pw>
2021-01-21 21:47:34 +0100

    feat: update linting rules, move typescript
    moved to @tmware/eslint-config-typescript

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ['standard'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'standard'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
@@ -16,8 +14,19 @@ module.exports = {
   },
   rules: {
     'no-console': 1,
-    'no-unused-vars': 0,
-    curly: ['warn', 'multi-line'],
-    'brace-style': ['warn', 'stroustrup', { allowSingleLine: true }]
+    // "no-unused-vars": 0,
+    curly: [2, 'multi-line'],
+    'brace-style': [2, 'stroustrup', { allowSingleLine: true }],
+    'no-await-in-loop': 1,
+    'no-extra-parens': 1,
+    'block-scoped-var': 2,
+    // "class-methods-use-this": 1,
+    'no-mixed-spaces-and-tabs': 2,
+    'array-bracket-newline': [2, { multiline: true }],
+    'no-void': [2, { allowAsStatement: true }],
+    'require-await': 0,
+    'eol-last': [2, 'always'],
+    'linebreak-style': [2, 'unix'],
+    indent: [2, 2]
   }
 }
